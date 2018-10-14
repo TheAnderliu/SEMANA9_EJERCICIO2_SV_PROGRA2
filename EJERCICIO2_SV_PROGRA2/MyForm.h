8119fd408d53a68331dfc507628bab6030a25db8
TheAnderliu <aldoanderson@gmail.com>
2018-10-13 21:39:45 -0500

    Primer commit

---
+++ EJERCICIO2_SV_PROGRA2/MyForm.h
@@ -0,0 +1,85 @@
+#pragma once
+
+namespace EJERCICIO2_SV_PROGRA2 {
+
+	using namespace System;
+	using namespace System::ComponentModel;
+	using namespace System::Collections;
+	using namespace System::Windows::Forms;
+	using namespace System::Data;
+	using namespace System::Drawing;
+
+	/// <summary>
+	/// Resumen de MyForm
+	/// </summary>
+	public ref class MyForm : public System::Windows::Forms::Form
+	{
+	public:
+		MyForm(void)
+		{
+			InitializeComponent();
+			//
+			//TODO: agregar código de constructor aquí
+			//
+		}
+
+	protected:
+		/// <summary>
+		/// Limpiar los recursos que se estén utilizando.
+		/// </summary>
+		~MyForm()
+		{
+			if (components)
+			{
+				delete components;
+			}
+		}
+
+	private:
+		/// <summary>
+		/// Variable del diseñador requerida.
+		/// </summary>
+		System::ComponentModel::Container ^components;
+
+#pragma region Windows Form Designer generated code
+		/// <summary>
+		/// Método necesario para admitir el Diseñador. No se puede modificar
+		/// el contenido del método con el editor de código.
+		/// </summary>
+		void InitializeComponent(void)
+		{
+			this->SuspendLayout();
+			// 
+			// MyForm
+			// 
+			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
+			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->ClientSize = System::Drawing::Size(467, 445);
+			this->IsMdiContainer = true;
+			this->Name = L"MyForm";
+			this->Text = L"MyForm";
+			this->Load += gcnew System::EventHandler(this, &MyForm::MyForm_Load);
+			this->ResumeLayout(false);
+
+		}
+#pragma endregion
+	private: System::Void MyForm_Load(System::Object^  sender, System::EventArgs^  e) {
+
+				 Form ^frm = gcnew Form();
+				 frm->MdiParent = this;
+				 frm->BackColor = Color::Yellow;
+				 frm->Left = 500;
+				 frm->Top = 500;
+				 frm->Show();
+
+				 Form ^frm2 = gcnew Form();
+				 frm2->MdiParent = this;
+				 frm2->BackColor = Color::DarkGoldenrod;
+				 frm2->Left = 300;
+				 frm2->Top = 300;
+				 frm2->Show();
+				 
+				// LayoutMdi(MdiLayout::Cascade);
+	}
+	};
+}
